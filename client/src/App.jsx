3a3ccdabc6d5796ef57623032325f4814fb044c6
chandan1971 <chandandey1357@gmail.com>
2024-01-21 16:11:46 +0530

    Routes Added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import { BrowserRouter,Route,Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
+import Projects from './pages/Projects'
 
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home></Home>} ></Route>
+        <Route path='/about' element={<About></About>} ></Route>
+        <Route path='/sign-up' element={<SignUp></SignUp>} ></Route>
+        <Route path='/sign-in'element={<SignIn></SignIn>} ></Route>
+        <Route path='/projects' elements={<Projects></Projects>}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
