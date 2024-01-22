6623502ad25dcac86b5a43fa56fb9e3e7b752d9f
chandan1971 <chandandey1357@gmail.com>
2024-01-22 17:35:09 +0530

    Header Section Added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,12 @@ import About from './pages/About'
 import SignUp from './pages/SignUp'
 import SignIn from './pages/SignIn'
 import Projects from './pages/Projects'
+import Header from './Components/Header'
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header></Header>
       <Routes>
         <Route path='/' element={<Home></Home>} ></Route>
         <Route path='/about' element={<About></About>} ></Route>
