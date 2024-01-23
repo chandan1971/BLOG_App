359c9ed050c497efa61970b58b9ef4a55ea5f982
chandan1971 <chandandey1357@gmail.com>
2024-01-23 15:42:30 +0530

    MongoDb Connected

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
 const express=require('express')
+const mongoose=require('mongoose')
+require('dotenv').config()
 
 const app=express();
 
 app.listen(3000,()=>{
     console.log(`Server is running on port 3000`);
 })
+
+mongoose
+.connect(process.env.MONGO_URI)
+.then(()=>{
+    console.log(`MongoDb Connected`);
+})
+.catch((err)=>{
+    console.log(err);
+})
+
