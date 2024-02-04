b338a5eac990511dfb1a3ac54ad4eedd423c4529
chandan1971 <chandandey1357@gmail.com>
2024-02-04 14:47:46 +0530

    Dark Mode functionality Added

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -2,13 +2,18 @@ import { Navbar, TextInput,Button, Dropdown, Avatar, DropdownDivider } from 'flo
 import React from 'react'
 import {Link, useLocation} from "react-router-dom"
 import {AiOutlineSearch} from "react-icons/ai"
-import {FaMoon} from "react-icons/fa"
+import {FaMoon,FaSun} from "react-icons/fa"
 import {useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
+import { toggleTheme } from '../redux/theme/themeSlice'
 
 
 function Header() {
     const path=useLocation().pathname;
     const {currentUser}=useSelector(state=>state.user)
+    const dispatch=useDispatch()
+    const {theme}=useSelector(state=>state.theme)
+
   return (
     <Navbar className='border-b-2'>
         <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
@@ -29,7 +34,9 @@ function Header() {
             <AiOutlineSearch></AiOutlineSearch>
         </Button>
         <div className='flex gap-2 md:order-2'>
-            <Button className='w-12 h-10 sm:inline' color='grey' pill><FaMoon></FaMoon></Button>     
+            <Button className='w-12 h-10 sm:inline' color='grey' pill onClick={()=>{
+                dispatch(toggleTheme())
+            }}>{theme ==='light' ? <FaMoon></FaMoon>:<FaSun></FaSun>}</Button>     
             {
                 currentUser ? (
                     <Dropdown
